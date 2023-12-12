13024962b9dd1a6ee43debb3feb8f5fee1bbc765
Abdul-SubhanL <AbdulSubhan.Latif@Arup.com>
2023-12-12 00:27:14 +0000

    changed codecov

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   testEnvironment: 'jsdom',
+  collectCoverage: true,
+  coverageReporters: ['text', 'cobertura'],
 };
